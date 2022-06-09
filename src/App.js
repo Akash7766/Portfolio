20e49fb58930c4236d65f7513b42d2a964664c65
akash7766 <mraakash01932@gmail.com>
2022-06-09 21:21:14 +0600

    about me part add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import Dashboard from "./Pages/Dashboard/Dashboard";
+import About from "./Pages/Home/About/About";
 import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
+      <div class="navbar bg-[#17181B] lg:hidden">
+        <div className="container mx-auto">
+          <div class="flex-1">
+            <Link to="/" className="btn btn-ghost normal-case text-xl">
+              Akash Rahman
+            </Link>
+          </div>
+          <div class="flex-none">
+            <button class="btn btn-square btn-ghost">
+              <label
+                for="my-drawer-2"
+                class="btn bg-transparent outline-none border-none hover:bg-transparent drawer-button lg:hidden"
+              >
+                <i class="fa-solid fa-bars text-xl"></i>
+              </label>
+            </button>
+          </div>
+        </div>
+      </div>
       <Routes>
         <Route path="/" element={<Dashboard></Dashboard>}>
           <Route index element={<Home></Home>}></Route>
           <Route path="home" element={<Home></Home>}></Route>
+          <Route path="about" element={<About></About>}></Route>
         </Route>
       </Routes>
     </div>
---
+++ src/Pages/Home/About/About.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <div className="py-10 mx-16">
+        <h2 className="text-5xl text-center border-b-2 pb-3 text-white">
+          <i class="fa-solid fa-user-tie mr-3"></i>About Me
+        </h2>
+      </div>
+      <div className="grid grid-flow-col px-14 gap-8 text-white">
+        <div className="col-span-8">
+          <h2 className="text-2xl font-bold mb-8 text-white">
+            Frontend Web Developer.
+          </h2>
+          <p>
+            Dolor reprehenderit aut repudiandae maiores recusandae. Consequatur
+            dolor nisi labore commodi ipsa? Excepturi velit laboriosam at
+            voluptatibus cupiditate est vel. Dolorem praesentium itaque fugiat
+            placeat quas consequatur? Obcaecati natus rerum Consectetur
+            consectetur nesciunt nesciunt blanditiis amet? <br /> <br /> Quod
+            quis unde aliquid excepturi laudantium? Illo architecto aliquam
+            velit illo magnam neque perferendis sint Incidunt eius ipsa
+            inventore labore eos velit. Minus hic magni reiciendis
+            necessitatibus ex Quidem officia deleniti accusamus obcaecati
+            dolores. Porro culpa quibusdam impedit porro praesentium excepturi.
+            Alias ab consequatur?
+          </p>
+        </div>
+        <div className="col-span-4">
+          <h2 className="text-2xl mb-5 font-bold">Personal Information</h2>
+          <p>Name: Minhazur Rahman Akash</p>
+          <p>Age: 22 Years</p>
+          <p>Country: Bangladesh</p>
+          <p>Address: Tejgaon, Dhaka 1208</p>
+          <p>Email: minhazur.akash1@gmail.com</p>
+          <p>Freelance: Available</p>
+          <button className="btn btn-outline btn-success mt-8">
+            Download Resume
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
