b0d482190f71185eddf8e4adad3311542382dd02
akash7766 <mraakash01932@gmail.com>
2022-06-11 01:52:32 +0600

    product details route demo implement

--- src/Pages/Portfolio/ProjectDetails.js
+++ src/Pages/Portfolio/ProjectDetails.js
@@ -1,13 +1,88 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import useProjects from "../../Hooks/useProjects";
 
 const ProjectDetails = () => {
   const { _id } = useParams();
-  const [projects] = useProjects();
-  console.log(projects);
-
-  return <div>This is project details for {_id}</div>;
+  const [project, setProject] = useState([]);
+  useEffect(() => {
+    fetch("/projects.json")
+      .then((res) => res.json())
+      .then((data) => setProject(data[_id - 1]));
+  }, []);
+  console.log(project);
+  return (
+    <div>
+      <div className="py-10 mx-5 md:mx-16">
+        <h2 className="text-3xl sm:text-5xl text-center border-b-2 pb-3 text-info">
+          {project.name}
+        </h2>
+        <h2 className="text-2xl text-white my-5">Uses Technology</h2>
+        <div className="flex gap-5 flex-wrap">
+          <span className="btn btn-info rounded-none cursor-default">
+            HTML5
+          </span>
+          <span className="btn btn-primary rounded-none cursor-default">
+            CSS3
+          </span>
+          <span className="btn btn-warning rounded-none cursor-default">
+            JavaScript
+          </span>
+          <span className="btn btn-secondary rounded-none cursor-default">
+            ReactJs
+          </span>
+          <span className="btn btn-success rounded-none cursor-default">
+            NodeJs
+          </span>
+          <span className="btn btn-accent rounded-none cursor-default">
+            Express Js
+          </span>
+          <span className="btn btn-error rounded-none cursor-default">
+            MongoDB
+          </span>
+        </div>
+        <div className="my-10">
+          <h2 className="text-2xl text-white my-5">Important Links -</h2>
+          <div className="flex gap-5 flex-wrap">
+            <a href={project.live_link} target="_blank" rel="noreferrer">
+              <button className="btn btn-outline btn-info rounded-none">
+                Live Site
+              </button>
+            </a>
+            <a href={project.git_client} target="_blank" rel="noreferrer">
+              <button className="btn btn-outline btn-warning rounded-none">
+                Github client
+              </button>
+            </a>
+            {project.git_server && (
+              <a href={project.git_server} target="_blank" rel="noreferrer">
+                <button className="btn btn-outline btn-secondary rounded-none">
+                  Github Server
+                </button>
+              </a>
+            )}
+          </div>
+        </div>
+        <div className="my-10">
+          <h2 className="text-2xl text-white my-5">
+            Some Screenshot from{" "}
+            <span className="text-info">{project.name}</span>
+          </h2>
+          <div className="grid md:grid-cols-2 gap-10">
+            <div className="bg-info p-3">
+              <img src={project.image1} alt="" />
+            </div>
+            <div className="bg-info p-3">
+              <img src={project.image1} alt="" />
+            </div>
+            <div className="bg-info p-3">
+              <img src={project.image1} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectDetails;
--- src/Pages/Resume/Resume.js
+++ src/Pages/Resume/Resume.js
@@ -79,33 +79,6 @@ const Resume = () => {
           </div>
         </div>
       </div>
-      {/* <div className="bg-white mx-10">
-        <progress
-          class="progress progress-primary w-56"
-          value="0"
-          max="100"
-        ></progress>
-        <progress
-          class="progress progress-primary w-56"
-          value="10"
-          max="100"
-        ></progress>
-        <progress
-          class="progress progress-primary w-56"
-          value="40"
-          max="100"
-        ></progress>
-        <progress
-          class="progress progress-primary w-56"
-          value="70"
-          max="100"
-        ></progress>
-        <progress
-          class="progress progress-primary w-56"
-          value="100"
-          max="100"
-        ></progress>
-      </div> */}
     </div>
   );
 };
