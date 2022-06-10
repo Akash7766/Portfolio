d96bd72602c52ef56ee0579cca1d67eb495014a7
akash7766 <mraakash01932@gmail.com>
2022-06-10 20:17:31 +0600

    portfolio page created with some fake data

---
+++ src/Pages/Portfolio/Projects.js
@@ -0,0 +1,21 @@
+import React from "react";
+import style from "./Projects.module.css";
+
+const Projects = ({ project }) => {
+  const { name, _id, image1 } = project;
+  return (
+    <div className={`${style.projects}`}>
+      <img src={image1} alt="" />
+      <div className={`${style.projects_title}`}>
+        <div className="text-center">
+          <h2 className="text-2xl mb-5">{name}</h2>
+          <button className="btn btn-outline btn-info rounded-none uppercase">
+            Project Details
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
